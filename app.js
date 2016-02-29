438b21163df2d5cb40a487cf3db3b85cf327f8cb
hendouf <hendoufahmed@gmail.com>
2016-02-29 19:08:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,111 +1,53 @@
-var app = angular.module('reddit-clone', ['ngRoute', 'firebase']);
+var app = angular.module("myApp", []);
 
-//The Firebase URL set as a Constant
-app.constant('fbURL', 'https://unknown-chat.firebaseio.com');
+app.service("newsService", function ($http, $q)
+{
+	var deferred = $q.defer();
+	$http.get('http://adneom.herokuapp.com/api/posts').then(function (data)
+	{
+		deferred.resolve(data);
+	});
 
-//Creating a factory so we can use the Firebase URL
-app.factory('Posts', function ($firebase, fbURL) {
-    return $firebase(new Firebase(fbURL)).$asArray();
-});
+	this.getNews = function ()
+	{
+		return deferred.promise;
+	}
+})
 
-//Configuring the route providers to redirect to the right location
-app.config(function ($routeProvider) {
-    $routeProvider
-        .when('/', {
-            controller: 'MainController',
-            templateUrl: 'main.html'
-        })
-        .otherwise({
-            redirectTo: '/'
-        })
-});
 
-//The Main Controller that holds everything
-app.controller('MainController', function ($scope, $firebase, Posts) {
 
-    //Set the posts we get to a global variable that can be used
-    $scope.posts = Posts;
 
-    //The function that runs when the user saves a post
-    $scope.savePost = function (post) {
-        if (post.name && post.description && post.url ) {
-            //Actually adding the posts to the Firebase
-            Posts.$add({
-                //Setting the post name
-                name: post.name,
-                //Setting the post description
-                description: post.description,
-                //Setting the post URL
-                url: post.url,
-                //Setting the post votes
-                votes: 0,
-                //Getting the current user
-               // user: $scope.authData.twitter.username
-            });
+app.controller("MainController", function ($scope, newsService)
+{
+	var promise = newsService.getNews();
+	promise.then(function (data)
+	{
+		$scope.News = data.data;
+		console.log($scope.News);
+	});
+	   $scope.newsservices = newsService;
+	   
+$scope.savePost = function(){	
 
-            //Resetting all the values
-            post.name = "";
-            post.description = "";
-            post.url = "";
-        } else {
-            //An alert to tell the user to log in or put something in all the fields
-            alert('Sorry bro, you need all of those inputs to be filled or you need to be logged in!')
-        }
-    }
+	$scope.News.push({ 'title':$scope.title, 'link': $scope.link});
+	$scope.title='';
+	$scope.link='';
+	
+};
+   
+
+})
+
 
     //Adding a vote
     $scope.addVote = function (post) {
         //Increment the number
         post.votes++;
         //Save to the Firebase
-        Posts.$save(post);
+        News.$save(post);
     }
 
-    //Deleting a post
-    $scope.deletePost = function (post) {
-        //Getting the right URL
-        var postForDeletion = new Firebase('https://unknown-chat.firebaseio.com/' + post.$id);
-        //Removing it from Firebase
-        postForDeletion.remove();
-    }
+   
 
-    $scope.addComment = function (post, comment) {
-      //  if ($scope.authData) {
-            var ref = new Firebase('https://unknown-chat.firebaseio.com/' + post.$id + '/comments');
-            var sync = $firebase(ref);
-            $scope.comments = sync.$asArray();
-            $scope.comments.$add({
-               // author: comment.author,
-                text: comment.text,
-            });
-      //  } else {
-           // alert('You need to be logged in before doing that!')
-      //  }
+   
 
-       comment.text = "";
-    }
-    
-    $scope.removeComment = function(post, comment) {
-        var commentForDeletion = new Firebase('https://unknown-chat.firebaseio.com/' + post.$id + '/comments/' + comment.$id);
-        commentForDeletion.remove();
-    }
-
-    //Logging the user in
-    $scope.login = function () {
-        //Creating a refrence URL with Firebase
-        var ref = new Firebase('https://unknown-chat.firebaseio.com/');
-        //Doing the OAuth popup
-        ref.authWithOAuthPopup('twitter', function (error, authData) {
-            //If there is an error
-            if (error) {
-                alert('Sorry bro, there was an error.');
-            }
-            //If the user is logged in correctly
-            else {
-                alert('You were logged in successfully.');
-            }
-            //Set the authData we get to a global variable that can be used
-            $scope.authData = authData;
-        });
-    }
-});
